f31dd775cc9234ca5a9ec9acdf3fbb6232fc2efa
mustafaessa8 <43000997+mustafaessa8@users.noreply.github.com>
2020-07-02 05:32:30 +0300

    Add files via upload

---
+++ Connect.php
@@ -0,0 +1,12 @@
+<<?php 
+$connect = mysqli_connect('localhost', 'root', '','remote_control');
+
+
+if(mysqli_connect_errno()){
+die("cannot connect to database " . mysqli_connect_error());
+
+}else{
+  echo  '';
+}
+
+?>
---
+++ Database.php
@@ -0,0 +1,27 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "remote_control";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql = "INSERT INTO `remote`(`Forward`, `Stop`, `Right`, `Left`) VALUES ('F','S', 'R', 'L')
+";
+
+if (mysqli_query($conn, $sql)) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+
+
+
+?>
